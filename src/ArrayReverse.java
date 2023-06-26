dcca6e0a5bc61da9ed8f6a0d326857172f31a1c0
Viktor Dimitrov <viktordimitrov3435@gmail.com>
2023-06-26 18:15:33 +0300

    ArrayReverse

--- src/ArrayReverse.java
+++ src/ArrayReverse.java
@@ -2,37 +2,33 @@ import java.util.Arrays;
 import java.util.Scanner;
 import java.util.stream.Collectors;
 
+//This utility class reverses an array
 public class ArrayReverse {
     public static void main(String[] args) {
-        /* reading the array */
         Scanner scan = new Scanner(System.in);
-        int[] array = Arrays.stream(scan.nextLine()         /* reading the array elements from the console */
+        int[] array = Arrays.stream(scan.nextLine()
                         .split(", "))
-                        .mapToInt(Integer::parseInt)
-                        .toArray();
-
-        /* reversing the array */
+                .mapToInt(Integer::parseInt)
+                .toArray();
         reverseArray(array);
-
         String arrayString = Arrays.stream(array)
                 .mapToObj(String::valueOf)
                 .collect(Collectors.joining(", "));
 
-        /* printing the array */
         System.out.println(arrayString);
     }
-    /* method for reversing the array */
-    public static void reverseArray(int[] array) {
-        int firstElement = 0;                  /* temporary variable indexing the start of the array */
-        int lastElement = array.length - 1;    /* temporary variable indexing the end of the array */
 
+    /**
+     * reverses the array
+     * @param array
+     **/
+    public static void reverseArray(int[] array) {
+        int firstElement = 0;
+        int lastElement = array.length - 1;
         while (firstElement < lastElement) {
-            /* Swap elements */
             int temp = array[firstElement];
             array[firstElement] = array[lastElement];
             array[lastElement] = temp;
-
-            /* Moving the indices towards the center of the array */
             firstElement++;
             lastElement--;
         }
