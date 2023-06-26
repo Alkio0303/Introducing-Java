74e60ea8bde5684cf38fa38add8e19c459923f5a
Viktor Dimitrov <viktordimitrov3435@gmail.com>
2023-06-26 18:44:58 +0300

    Tests

--- test/src/ArrayProcessingTest.java
+++ test/src/ArrayProcessingTest.java
@@ -1,8 +1,5 @@
 package src;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 public class ArrayProcessingTest {
     @Test
     public void minElementIndexTest(){
